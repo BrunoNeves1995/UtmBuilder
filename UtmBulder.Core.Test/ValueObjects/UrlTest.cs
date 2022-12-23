4673ebca5dfa9ffaecca05187d8a1c2e5dd7ec5f
Bruno Neves <nevesbruno814@gmail.com>
2022-12-23 14:11:15 -0400

    teste implementado com sucesso

--- UtmBulder.Core.Test/ValueObjects/UrlTest.cs
+++ UtmBulder.Core.Test/ValueObjects/UrlTest.cs
@@ -6,6 +6,8 @@ namespace UtmBulder.Core.Test.ValueObjects
     [TestClass]
     public class UrlTest
     {
+        private const string urlInvalida = "http//www.bnana?banana?cocoa&banaca";
+        private const string urlValida = "https://balta.io/";
 
         [TestMethod]
         [ExpectedException(typeof(InvalidUrlException))]
@@ -22,14 +24,49 @@ namespace UtmBulder.Core.Test.ValueObjects
             //   Assert.IsTrue(true);
             //}
 
-           new Url(address: "http//www.bnana?banana?cocoa&banaca");
+           new Url(address: urlInvalida);
         }
 
         [TestMethod]
         public void Dado_uma_url_valida_nao_deve_retorna_uma_execao()
         {
-            new Url(address: "https://balta.io/");
+            new Url(address: urlValida);
             Assert.IsTrue(true);
+
+        }
+
+        [TestMethod]
+        [DataRow("https//www.bnana?banana?cocoa&banaca", moreData: true)] // to esperando uma exeção
+        [DataRow("http/banana.com.br", moreData: true)]
+        [DataRow("banana", moreData: true)]
+        [DataRow("", moreData: true)]
+        [DataRow("https://balta.io/", moreData: false)] // não to esperando uma exeção
+        public void TesteDataRow(
+            string link,
+            bool ExpectException)
+        {
+            if (ExpectException)
+            {
+                try
+                {
+                    new Url(address: link);
+                    Assert.Fail();
+
+                }
+                catch (InvalidUrlException)
+                {
+
+                    Assert.IsTrue(true);
+                }
+            }
+            else
+            {
+                new Url(address: link);
+                Assert.IsTrue(true);
+            }
+
+
+
         }
     }
 }
