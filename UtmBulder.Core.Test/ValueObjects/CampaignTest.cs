4a2b81d455159f80aa99ca720a2cc9b306c11af9
Bruno Neves <nevesbruno814@gmail.com>
2022-12-24 09:07:52 -0400

    teste da campaign implementado com sucesso

---
+++ UtmBulder.Core.Test/ValueObjects/CampaignTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBulder.Core.Test.ValueObjects
+{
+    [TestClass]
+    public class CampaignTest
+    {
+        [TestMethod]
+        [DataRow("", "", "", true)]
+        [DataRow("source", "", "", true)]
+        [DataRow("source", "medium", "", true)]
+        [DataRow("", "medium", "", true)]
+        [DataRow("", "", "name", true)]
+        [DataRow("source", "medium", "name", false)]
+        public void TestCampaign(
+            string source,
+            string medium,
+            string name,
+            bool expectException)
+        {
+            if(expectException)
+            {
+                try
+                {
+                    new Campaign(source, medium, name);
+                    Assert.Fail();
+                }
+                catch (InvalidCampaignException)
+                {
+
+                    Assert.IsTrue(true);
+                }
+            }
+            else
+            {
+                new Campaign(source, medium, name);
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
